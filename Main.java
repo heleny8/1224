65dbc71ed2ff2bb8b7a9ae83ce7fa86a9fcbe3bc
HELENY1 <16840644-HELENY1@users.noreply.replit.com>
2024-01-02 13:37:23 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,15 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Can't wait for regents week!");
+    System.out.println("Need another day off school...");
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
